59bcc648e6a8976bffb23c4cd3cac09e329ce0e9
Javier Rojas <jerojasro@devnull.li>
2013-01-07 19:44:51 -0500

    initial rails files

---
+++ holidays/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holidays::Application.config.secret_token = 'a4ccca37e0d66320e39d5f1bf32922abedb3d87fd743dab12b826c23304e18b17c3096dcae80c744d64c5f81d0c5ec882aca4cb1f661d519fb87a5da592ed517'
